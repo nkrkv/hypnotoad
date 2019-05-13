0903cc2cbbcebd23fe25feca6be610cfd2bb040e
Victor Nakoryakov <nail.xx@gmail.com>
2019-05-13 17:05:37 +0300

    add eye control buttons

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const MQTT_BROKER_URL = 'ws://localhost:9001';
 
 const client = mqtt.connect(MQTT_BROKER_URL);
 
-setInterval(() => {
-  client.publish('toad', 'hello');
-}, 5000);
+function onEyeControlClick() {
+  const { queue, payload } = this.dataset;
+  client.publish(`toad/eye/${queue}`, payload);
+}
+
+document
+  .querySelectorAll(".toad__eye__control")
+  .forEach(e => e.addEventListener('click', onEyeControlClick));
