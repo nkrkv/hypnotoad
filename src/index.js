fb1fe1c944504de40daf7e6069eba1569469e1e0
Victor Nakoryakov <nail.xx@gmail.com>
2019-05-13 16:18:41 +0300

    feat: run mosquitto along with `yarn start`

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("hello index");
+import mqtt from 'mqtt';
+
+const MQTT_BROKER_URL = 'ws://localhost:9001';
+
+const client = mqtt.connect(MQTT_BROKER_URL);
+
+setInterval(() => {
+  client.publish('toad', 'hello');
+}, 5000);
